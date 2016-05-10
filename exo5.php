f0ec267b807e62ce9ef4d7562b16f8dfa19a62b1
maite1 <maitegerard1@gmail.com>
2016-05-10 14:17:33 +0200

    renvoie du fichier

--- exo5.php
+++ exo5.php
@@ -2,7 +2,7 @@
 $alpha = "c'est ta boite a malice ?";
  $beta = "non! ma boite a canif !!" ;
   function fusion($alpha,$beta){
-   return $alpha.$beta;
+   return $alpha . $beta;
 }
-echo $alpha.$beta;
+echo fusion ($alpha , $beta);
 ?>
